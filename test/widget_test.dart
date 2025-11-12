786a52a31a8462dcc6dc763796ee690f78741501
Arijit Kundu <arikundu9@gmail.com>
2025-11-12 15:32:10 +0530

    Migrate app to use Fluent UI package and update login page layout

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:my_flutter_app_0/main.dart';
@@ -20,7 +20,7 @@ void main() {
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(FluentIcons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
